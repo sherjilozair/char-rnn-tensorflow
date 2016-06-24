b4c4888d6ea4a48a8e73475c8c1ad20066e1a348
John Parton <john.parton@plushrugs.com>
2016-06-24 08:25:17 -0500

    Make sample play nicer with unicode.

--- sample.py
+++ sample.py
@@ -10,13 +10,15 @@ from six.moves import cPickle
 from utils import TextLoader
 from model import Model
 
+from six import text_type
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--save_dir', type=str, default='save',
                        help='model directory to store checkpointed models')
     parser.add_argument('-n', type=int, default=500,
                        help='number of characters to sample')
-    parser.add_argument('--prime', type=str, default=' ',
+    parser.add_argument('--prime', type=text_type, default=u' ',
                        help='prime text')
     parser.add_argument('--sample', type=int, default=1,
                        help='0 to use max at each timestep, 1 to sample at each timestep, 2 to sample on spaces')
