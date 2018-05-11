e08068eed1c208f9108d56156149217f29d1e2f6
Sherjil Ozair <sherjilozair@gmail.com>
2018-05-11 18:55:41 -0400

    do the same for sample.py

--- sample.py
+++ sample.py
@@ -1,33 +1,31 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import tensorflow as tf
 
 import argparse
 import os
 from six.moves import cPickle
 
-from model import Model
 
 from six import text_type
 
 
-def main():
-    parser = argparse.ArgumentParser(
-                       formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--save_dir', type=str, default='save',
-                        help='model directory to store checkpointed models')
-    parser.add_argument('-n', type=int, default=500,
-                        help='number of characters to sample')
-    parser.add_argument('--prime', type=text_type, default=u'',
-                        help='prime text')
-    parser.add_argument('--sample', type=int, default=1,
-                        help='0 to use max at each timestep, 1 to sample at '
-                             'each timestep, 2 to sample on spaces')
+parser = argparse.ArgumentParser(
+                   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--save_dir', type=str, default='save',
+                    help='model directory to store checkpointed models')
+parser.add_argument('-n', type=int, default=500,
+                    help='number of characters to sample')
+parser.add_argument('--prime', type=text_type, default=u'',
+                    help='prime text')
+parser.add_argument('--sample', type=int, default=1,
+                    help='0 to use max at each timestep, 1 to sample at '
+                         'each timestep, 2 to sample on spaces')
 
-    args = parser.parse_args()
-    sample(args)
+args = parser.parse_args()
 
+import tensorflow as tf
+from model import Model
 
 def sample(args):
     with open(os.path.join(args.save_dir, 'config.pkl'), 'rb') as f:
@@ -48,4 +46,4 @@ def sample(args):
                                args.sample).encode('utf-8'))
 
 if __name__ == '__main__':
-    main()
+    sample(args)
