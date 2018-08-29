53ba6ce3054d70bbbd6d5a6de9c9fdde30453fa8
Aditya Sharma <adityashrm21@gmail.com>
2018-08-28 22:52:17 -0700

    decoding again from the utf-8 format
    This is to make the output more readable and assessable.

--- sample.py
+++ sample.py
@@ -42,8 +42,9 @@ def sample(args):
         ckpt = tf.train.get_checkpoint_state(args.save_dir)
         if ckpt and ckpt.model_checkpoint_path:
             saver.restore(sess, ckpt.model_checkpoint_path)
-            print(model.sample(sess, chars, vocab, args.n, args.prime,
-                               args.sample).encode('utf-8'))
+            data = model.sample(sess, chars, vocab, args.n, args.prime,
+                               args.sample).encode('utf-8')
+            print(data.decode("utf-8"))
 
 if __name__ == '__main__':
     sample(args)
